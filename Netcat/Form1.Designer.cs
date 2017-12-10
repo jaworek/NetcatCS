59bc70da5ec62f05879e46fbb82684723aa755e8
Jan Jaworski <jan.jaworski16@my.northampton.ac.uk>
2017-12-10 17:16:31 +0000

    Initial commit

---
+++ Netcat/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace Netcat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(26, 25);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(770, 462);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(26, 511);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(6);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(770, 31);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyDown);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(812, 62);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(6);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(196, 31);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(812, 137);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(6);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(196, 31);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(812, 212);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(6);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(196, 31);
+            this.textBox5.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(812, 31);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(177, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Remote Address:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(812, 106);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Remote Port:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(812, 181);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Local Port:";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Location = new System.Drawing.Point(0, 24);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(12, 4, 0, 4);
+            this.menuStrip1.Size = new System.Drawing.Size(1052, 24);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Padding = new System.Windows.Forms.Padding(12, 4, 0, 4);
+            this.menuStrip2.Size = new System.Drawing.Size(1052, 24);
+            this.menuStrip2.TabIndex = 9;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Items.AddRange(new object[] {
+            "TCP Server",
+            "TCP Client",
+            "UDP Server",
+            "UDP Client"});
+            this.listBox1.Location = new System.Drawing.Point(814, 294);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(194, 104);
+            this.listBox1.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(812, 263);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 25);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Role:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(812, 446);
+            this.button1.Margin = new System.Windows.Forms.Padding(6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 44);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(812, 504);
+            this.button2.Margin = new System.Windows.Forms.Padding(6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 44);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Send";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1052, 557);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.menuStrip2);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Windows Netcat";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Netcat/Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+using System.Net;
+
+namespace Netcat
+{
+    public partial class Form1 : Form
+    {
+        delegate void SetTextCallback(string text);
+
+        private void SetText(string text)
+        {
+            if (this.textBox1.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.textBox1.Text = this.textBox1.Text + text + Environment.NewLine;
+            }
+        }
+
+        class WncTcpServer
+        {
+            TcpListener listener;
+            TcpClient client;
+            StreamReader sr;
+            StreamWriter sw;
+            Button b;
+            TextBox t1, t2;
+            SetTextCallback f;
+
+            public WncTcpServer(ref Button bt, ref TextBox tb1, ref TextBox tb2, String port, SetTextCallback ff)
+            {
+                listener = new TcpListener(IPAddress.Parse("127.0.0.1"), int.Parse(port));
+                listener.Start();
+
+                client = listener.AcceptTcpClient();
+
+                sr = new StreamReader(client.GetStream());
+                sw = new StreamWriter(client.GetStream());
+                sw.AutoFlush = true;
+
+                b = bt;
+                t1 = tb1;
+                t2 = tb2;
+                b.Click += tx;
+
+                f = ff;
+
+                Thread th = new Thread(rx);
+                th.Start();
+            }
+
+            public void tx(Object sender, EventArgs e)
+            {
+                String toClient;
+                toClient = t2.Text;
+                if (toClient != null) sw.WriteLine(toClient);
+                f("Me: " + toClient);
+                t2.Text = "";
+            }
+
+            void rx()
+            {
+                String fromServer;
+                do
+                {
+                    fromServer = sr.ReadLine();
+                    if (fromServer != null) f("Client: " + fromServer);
+                }
+                while (fromServer != null);
+
+                System.Environment.Exit(0);
+            }
+        }
+
+        class WncTcpClient
+        {
+            TcpClient client;
+            StreamReader sr;
+            StreamWriter sw;
+            Button b;
+            TextBox t1, t2;
+            SetTextCallback f;
+
+            public WncTcpClient(ref Button bt, ref TextBox tb1, ref TextBox tb2, String rip, String rport, SetTextCallback ff)
+            {
+                client = new TcpClient(rip, int.Parse(rport));
+
+                sr = new StreamReader(client.GetStream());
+                sw = new StreamWriter(client.GetStream());
+                sw.AutoFlush = true;
+
+                b = bt;
+                t1 = tb1;
+                t2 = tb2;
+                b.Click += tx;
+
+                f = ff;
+
+                Thread th = new Thread(rx);
+                th.Start();
+            }
+
+            public void tx(Object sender, EventArgs e)
+            {
+                String toClient;
+                toClient = t2.Text;
+                if (toClient != null) sw.WriteLine(toClient);
+                f("Me: " + toClient);
+                t2.Text = "";
+            }
+
+            void rx()
+            {
+                String fromServer;
+                do
+                {
+                    fromServer = sr.ReadLine();
+                    if (fromServer != null) f("Client: " + fromServer);
+                }
+                while (fromServer != null);
+
+                System.Environment.Exit(0);
+            }
+        }
+
+        class WncUdpServer
+        {
+            UdpClient client;
+            String ip = null;
+            int port;
+            Button b;
+            TextBox t1, t2;
+            SetTextCallback f;
+
+            public WncUdpServer(ref Button bt, ref TextBox tb1, ref TextBox tb2, String lport, SetTextCallback ff)
+            {
+                port = int.Parse(lport);
+                client = new UdpClient(port);
+
+                b = bt;
+                t1 = tb1;
+                t2 = tb2;
+                b.Click += tx;
+
+                f = ff;
+
+                Thread th = new Thread(rx);
+                th.Start();
+            }
+
+            public void tx(Object sender, EventArgs e)
+            {
+                String toClient;
+                toClient = t2.Text;
+                if (toClient != null) client.Send(Encoding.ASCII.GetBytes(toClient), toClient.Length, ip, port);
+                f("Me: " + toClient);
+                t2.Text = "";
+            }
+
+            void rx()
+            {
+                IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+                String fromServer;
+                do
+                {
+                    fromServer = Encoding.ASCII.GetString(client.Receive(ref RemoteIpEndPoint));
+                    ip = RemoteIpEndPoint.Address + "";
+                    port = RemoteIpEndPoint.Port;
+                    if (fromServer != null) f("Client: " + fromServer);
+                }
+                while (fromServer != null);
+
+                System.Environment.Exit(0);
+            }
+        }
+
+        class WncUdpClient
+        {
+            UdpClient client;
+            String ip = null;
+            int port;
+            Button b;
+            TextBox t1, t2;
+            SetTextCallback f;
+
+            public WncUdpClient(ref Button bt, ref TextBox tb1, ref TextBox tb2, String rip, String rport, SetTextCallback ff)
+            {
+                client = new UdpClient();
+
+                ip = rip;
+                port = int.Parse(rport);
+
+                b = bt;
+                t1 = tb1;
+                t2 = tb2;
+                b.Click += tx;
+
+                f = ff;
+
+                Thread th = new Thread(rx);
+                th.Start();
+            }
+
+            public void tx(Object sender, EventArgs e)
+            {
+                String toClient;
+                toClient = t2.Text;
+                if (toClient != null) client.Send(Encoding.ASCII.GetBytes(toClient), toClient.Length, ip, port);
+                f("Me: " + toClient);
+                t2.Text = "";
+            }
+
+            void rx()
+            {
+                IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                client.Client.Bind(RemoteIpEndPoint); // NOTE THIS LINE. THIS IS FOR THE UDP CLIENT ONLY
+
+                String fromServer;
+                do
+                {
+                    fromServer = Encoding.ASCII.GetString(client.Receive(ref RemoteIpEndPoint));
+                    ip = RemoteIpEndPoint.Address + "";
+                    port = RemoteIpEndPoint.Port;
+                    if (fromServer != null) f("Client: " + fromServer);
+                }
+                while (fromServer != null);
+
+                System.Environment.Exit(0);
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox3.Enabled = false;
+            textBox4.Enabled = false;
+            textBox5.Enabled = false;
+            listBox1.Enabled = false;
+            button1.Enabled = false;
+
+            if (listBox1.SelectedIndex == 0) new WncTcpServer(ref button2, ref textBox1, ref textBox2, textBox5.Text, SetText);
+            if (listBox1.SelectedIndex == 1) new WncTcpClient(ref button2, ref textBox1, ref textBox2, textBox3.Text, textBox4.Text, SetText);
+            if (listBox1.SelectedIndex == 2) new WncUdpServer(ref button2, ref textBox1, ref textBox2, textBox5.Text, SetText);
+            if (listBox1.SelectedIndex == 3) new WncUdpClient(ref button2, ref textBox1, ref textBox2, textBox3.Text, textBox4.Text, SetText);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //textBox1.Text = "Hello";
+        }
+
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                MessageBox.Show("Enter key pressed");
+            }
+        }
+    }
+}
